94bc1b21436de49cc95475e6fd244f004973dcc2
Sascha Block <Block@inztitut.de>
2021-09-20 19:44:55 +0200

    we are using a useCurrentUser hook in our AuthCluster component.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import {RecoilRoot} from "recoil"
+import {CurrentUserSubscription} from "./hooks/current-user"
 
 ReactDOM.render(
     <React.StrictMode>
         <RecoilRoot>
+            <CurrentUserSubscription />
             <App />
         </RecoilRoot>
     </React.StrictMode>,
